4603fdea5bbff57c72ffa846be4d448223efea36
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-01-31 01:28:01 -0500

    Adding Vectors

--- Module3_CreatingDataFrame.R
+++ Module3_CreatingDataFrame.R
@@ -9,3 +9,8 @@
 
 #CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
 
+#Creating the specififed vectors 
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABCpoll <- c(4, 62, 51, 21, 2, 14, 15)
+CBSpoll <- c(12, 75, 43, 19, 1, 21, 19)
+
