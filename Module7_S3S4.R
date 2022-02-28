261be9167301d79183754d73c01119056d77deaf
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-02-28 04:33:50 -0500

    creating s4 class

--- Module7_S3S4.R
+++ Module7_S3S4.R
@@ -12,8 +12,18 @@ library(pryr)
 otype(air)
 
 #Example of dataset for S3
-s3 <- list(name = "Myself", height = 25, weight = 140)
+s3 <- list(name = "Myself", height = 5.3 , weight = 140)
 s3
 
 s3_2 <- list(school = "USF", campus = "Tampa", state = "FL" )
 s3_2
+
+#Example of dataset for S4
+  #Defining S4 class
+setClass("Family_Dynamics",
+         representation(
+         name = "character",
+         height = "numeric",
+         weight = "numeric")
+)
+"Family_Dynamics"
