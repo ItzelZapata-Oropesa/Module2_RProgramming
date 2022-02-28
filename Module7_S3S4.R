d30095a00543d1ee9d1c29de1e17ee58836e0f97
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-02-28 04:23:59 -0500

    S3 personal example created

--- Module7_S3S4.R
+++ Module7_S3S4.R
@@ -10,3 +10,10 @@ library(pryr)
 
 #Checking the type of object 
 otype(air)
+
+#Example of dataset for S3
+s3 <- list(name = "Myself", height = 25, weight = 140)
+s3
+
+s3_2 <- list(school = "USF", campus = "Tampa", state = "FL" )
+s3_2
