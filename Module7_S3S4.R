f8011b61a7a1ea172afd3348aab12f7fc7dd110c
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-02-28 03:46:33 -0500

    removing unwated lines and moving code around

--- Module7_S3S4.R
+++ Module7_S3S4.R
@@ -1,13 +1,12 @@
-#install.packages("pryr")
-library(pryr)
-
 #Selecting Dataset
 air <- airquality
 
 #Exploring the data
 head(air)
 str(air)
-list(air)
+
+#install.packages("pryr")
+library(pryr)
 
 #Checking the type of object 
 otype(air)
