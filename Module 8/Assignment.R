17117e26ffb684d61af0b4417bab1f1591098a9d
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-03-07 02:10:56 -0500

    Using plyr to generate mean Grade split by gender

--- Module 8/Assignment.R
+++ Module 8/Assignment.R
@@ -8,3 +8,7 @@ library(data.table)
 studentAssignment6 <- read.table(file.choose("Assignment 6 dataset.txt"), header = TRUE ,sep = ",")
 studentAssignment6
 
+#Run plyr to generate mean of grade split by gender
+studentAverage <- ddply(studentAssignment6, "Sex", transform, Grade.Average = mean(Grade))
+
+
