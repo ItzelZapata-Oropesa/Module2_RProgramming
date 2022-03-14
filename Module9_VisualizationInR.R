a063b4c50fdbe3eacef0b574621bd4473903b1f6
Itzel Zapata-Oropesa <88976598+ItzelZapata-Oropesa@users.noreply.github.com>
2022-03-14 04:36:14 -0400

    plots

--- Module9_VisualizationInR.R
+++ Module9_VisualizationInR.R
@@ -1,8 +1,18 @@
-#China suicide data
+#Storm data
 
 #load data and save as a variable
-ChinaSuicide <- read.table("https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/SuicideChina.csv", header = TRUE, sep = ",")
+storms <- read.table("https://vincentarelbundock.github.io/Rdatasets/csv/dplyr/storms.csv", header = TRUE, sep = ",")
 
 #Removve first column and which contains the row numbers
-ChinaSuicide <- ChinaSuicide[ , -1]
+storms <- storms[ , -1]
 
+#Base R plot
+plot(x = as.factor(storms$status), y = storms$month, pch = 16, col = "green" ,
+     main = "Stroms 1975-2015 Base R", xlab = "Status", ylab = "Month")
+
+#lattice Plot
+library(lattice)
+
+latticePlot <- xyplot(month ~ as.factor(status), data = storms, cex = 1.5, group = status,
+                      auto.key = TRUE, main = "1975 - 2015 Storms Lattice", xlab = "Status")
+latticePlot
